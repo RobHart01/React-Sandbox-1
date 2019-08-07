45052ba981c8323b3d849ee213fa2f110bbf987c
Robert Hartley <robert.taku.hartley@gmail.com>
2019-08-07 06:02:43 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hey it's me ya boy Robin, just testing out and playing with React to have fun you know ah ha ha.
+    </div>
+  );
+}
+
+export default App;
